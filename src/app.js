a78d0df5d4c7ed0149667431207b44bd85f9d730
Francisco Riveros <friveros.icc@gmail.com>
2015-08-31 21:48:20 -0300

    implementada búsqueda de videos por texto

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 var angular = require('../vendor/angular/');
+require('./youtube');
 
-angular.module('zbox-challenge', [])
+angular.module('zbox-challenge', ['zbox-challenge.youtube'])
 
 /**
  * Controlador de la vista principal
  *
  * @author Francisco Riveros
  */
-.controller('AppController', ['$scope', 
-	function($scope) {
-		$scope.videos = [];		
+.controller('AppController', ['$scope', 'youtubeResource',
+	function($scope, youtubeResource) {
+		$scope.videos = [];
+
+		/**
+		 * Busca el listado de videos que coinciden con el 
+		 * texto del filtro.
+		 */
+		$scope.buscarVideos = function() {
+			youtubeResource.buscar($scope.filtro.text)
+			.then(function(response) {
+				$scope.videos = response.data.items;
+				$scope.filtro.nextPageToken = response.data.nextPageToken;
+
+				console.log($scope.filtro);
+			});
+		};
 	}
 ]);
---
+++ src/youtube.js
@@ -0,0 +1,22 @@
+(function(){
+	angular.module('zbox-challenge.youtube', [])
+	.factory('youtubeResource', ['$http', 
+		function($http) {
+			var _apiKey = 'AIzaSyBhNZXwjch93VAPABxeDLtP-NhtLIQpjlY';
+			var _url = 'https://www.googleapis.com/youtube/v3/search';		
+
+			function buscar(text, pageToken) {
+				var params = {
+					part 		: 'snippet',
+					q			: text,
+					key 		: _apiKey,
+					pageToken	: pageToken
+				};			
+
+				return $http.get(_url, { params : params });
+			}
+
+			return { buscar : buscar };
+		}
+	]);	
+})();
